d2126a9b3f4cc7c99a540e90b692b998520bdb81
Kisyoka7 <kisyokav@gmail.com>
2022-06-22 14:04:46 +0000

    second commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+function getInputValue() {
+    var inputVal = document.getElementById("input").value;
+  document.getElementById("message").innerHTML = "hello " +inputVal + "welcome to the bootcamp"
+  
+
+}
+   
